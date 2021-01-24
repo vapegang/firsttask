2b21051c51c679b1512ae47bb8f360329b67bbc9
2chevsky <42774540+vapegang@users.noreply.github.com>
2021-01-24 17:45:30 +0400

    Add files via upload

---
+++ helloworld/src/main/java/com/mycompany/helloworld/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.helloworld;
+
+public class Main {
+    public static void main(String[]args){
+            { System.out.println("Hello, my name is Daniil. I'm from Russia!");}
+    }
+    }
+
+/*
+Корабельников Даниил, 3ПКС-27.
+Практическая работа 1, прикладное программирование.
+*/
